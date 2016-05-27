1638618efc0f12102c442a8bb23ef92f7b128c76
mallikarjuna  j <mallikarjuna.j@vensi.com>
2016-05-27 23:09:22 +0530

    code clean

--- sw.js
+++ sw.js
@@ -1,35 +1,35 @@
 // Version 7
 
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('playbulb-candle').then(function(cache) {
-      return cache.addAll([
-        '/demos/playbulb-candle/',
-        '/demos/playbulb-candle/index.html',
-        '/demos/playbulb-candle/styles.css',
-        '/demos/playbulb-candle/playbulbCandle.js',
-        '/demos/playbulb-candle/app.js',
-        '/demos/playbulb-candle/color-wheel.png',
-        '/demos/playbulb-candle/code.getmdl.io/1.0.4/material.green-light_green.min.css',
-        '/demos/playbulb-candle/code.getmdl.io/1.0.4/material.min.js',
-
-        'https://fonts.googleapis.com/icon?family=Material+Icons',
-      ]).then(function() {
-        return self.skipWaiting();
-      });
-    })
-  );
+    e.waitUntil(
+        caches.open('playbulbcandle').then(function(cache) {
+            return cache.addAll([
+                '/demos/playbulb-candle-ba/',
+                '/demos/playbulb-candle-ba/index.html',
+                '/demos/playbulb-candle-ba/styles.css',
+                '/demos/playbulb-candle-ba/bower_components/gatt-ip-js/gattip.js',
+                '/demos/playbulb-candle-ba/wb-polyfill.js',
+                '/demos/playbulb-candle-ba/playbulbCandle.js',
+                '/demos/playbulb-candle-ba/app.js',
+                '/demos/playbulb-candle-ba/color-wheel.png',
+                '/demos/playbulb-candle-ba/code.getmdl.io/1.0.4/material.green-light_green.min.css',
+                '/demos/playbulb-candle-ba/code.getmdl.io/1.0.4/material.min.js',
+                'https://fonts.googleapis.com/icon?family=Material+Icons',
+            ]).then(function() {
+                return self.skipWaiting();
+            });
+        })
+    );
 });
 
 self.addEventListener('activate', function(event) {
-  event.waitUntil(self.clients.claim());
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
-
