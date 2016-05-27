242d845f91390dea29e79bdc8c38b1ed7febd14a
mallikarjuna  j <mallikarjuna.j@vensi.com>
2016-05-27 16:59:35 +0530

    Fix candle effect
    * Removed device information service
    
    * Fixed color effect reset

--- sw.js
+++ sw.js
@@ -1,19 +1,17 @@
-// Version 3
+// Version 7
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('playbulb-candle').then(function(cache) {
       return cache.addAll([
-        '/demos/playbulb-candle-ba/',
-        '/demos/playbulb-candle-ba/index.html',
-        '/demos/playbulb-candle-ba/styles.css',
-        '/demos/playbulb-candle-ba/bower_components/gatt-ip-js/gattip.js',
-        '/demos/playbulb-candle-ba/wb-polyfill.js',
-        '/demos/playbulb-candle-ba/playbulbCandle.js',
-        '/demos/playbulb-candle-ba/app.js',
-        '/demos/playbulb-candle-ba/color-wheel.png',
-        '/demos/playbulb-candle-ba/code.getmdl.io/1.0.4/material.green-light_green.min.css',
-        '/demos/playbulb-candle-ba/code.getmdl.io/1.0.4/material.min.js',
+        '/demos/playbulb-candle/',
+        '/demos/playbulb-candle/index.html',
+        '/demos/playbulb-candle/styles.css',
+        '/demos/playbulb-candle/playbulbCandle.js',
+        '/demos/playbulb-candle/app.js',
+        '/demos/playbulb-candle/color-wheel.png',
+        '/demos/playbulb-candle/code.getmdl.io/1.0.4/material.green-light_green.min.css',
+        '/demos/playbulb-candle/code.getmdl.io/1.0.4/material.min.js',
         'https://fonts.googleapis.com/icon?family=Material+Icons',
       ]).then(function() {
         return self.skipWaiting();
